27b5880a58b87c229f99a76ef731e730f1b29ec0
johnmart19 <johnivan19999@gmail.com>
2025-05-14 05:08:25 +0300

    Alioth: MiuiCamera: Move to device/xiaomi/camera

--- device.mk
+++ device.mk
@@ -10,8 +10,8 @@ TARGET_IS_VAB := true
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
-# Miuicamera
-$(call inherit-product, vendor/xiaomi/camera/miuicamera.mk)
+# Miui Camera
+include device/xiaomi/camera/miuicamera.mk
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
